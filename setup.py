d93e8986a84ef19d1f3040f25240bf25f6094b4c
sak96 <26397224+sak96@users.noreply.github.com>
2022-01-02 18:28:49 +0530

    Use setup.cfg instead of setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(
-    name="cobertura_merge",
-    version="0.1.0",
-    packages=find_packages(include=["cobertura_merge", "cobertura_merge.*"]),
-    entry_points={"console_scripts": ["cobertura-merge=cobertura_merge:main"]},
-    install_requires=["pydantic==1.8.2", "xmltodict==0.12.0"],
-    extras_require={"dev": ["nox"]},
-    license_files=("LICENSE",),
-    classifiers=[
-        "License :: OSI Approved :: MIT License",
-    ],
-)
+setup()
