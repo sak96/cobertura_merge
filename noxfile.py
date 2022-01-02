b1d669d84026083eae574565fca727a687d45320
sak96 <26397224+sak96@users.noreply.github.com>
2022-01-02 19:23:09 +0530

    nox always reuse venv

--- noxfile.py
+++ noxfile.py
@@ -7,7 +7,7 @@ ALLOWED_LICENSE = ["MIT License", "Python Software Foundation License"]
 ALLOWED_OUTDATED_PACKAGES = ["pip", "filelock", "platformdirs", "setuptools"]
 
 
-@session
+@session(reuse_venv=True)
 def doc(session):
     session.install("pycodestyle", "darglint")
     session.install(".")
@@ -15,14 +15,14 @@ def doc(session):
     session.run("darglint", "cobertura_merge")
 
 
-@session
+@session(reuse_venv=True)
 def license(session):
     session.install("pip-licenses")
     session.install(".")
     session.run("pip-licenses", "--allow-only", ";".join(ALLOWED_LICENSE))
 
 
-@session
+@session(reuse_venv=True)
 def lint(session):
     session.install("mypy", "isort", "black", "flake8", "mccabe", "pylint")
     session.install(".")
@@ -33,7 +33,7 @@ def lint(session):
     session.run("pylint", "cobertura_merge")
 
 
-@session
+@session(reuse_venv=True)
 def outdated(session):
     session.install(".")
     exclude_args = sum((["--exclude", pkg] for pkg in ALLOWED_OUTDATED_PACKAGES), [])
@@ -44,7 +44,7 @@ def outdated(session):
         raise Exception(output.decode())
 
 
-@session
+@session(reuse_venv=True)
 def security(session):
     session.install("bandit", "safety")
     session.install(".")
